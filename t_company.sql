6153009318b8ae36b17f61d2dd595b12ec84b74e
yellow-chicken <63347013+yellow-chicken@users.noreply.github.com>
2020-07-07 17:44:05 +0800

    Add files via upload

---
+++ t_company.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_company`(`id`, `username`, `name`, `tel`, `money`) VALUES (1, '300001', '蜂鸟快递', '15957745621', 0.00);
+INSERT INTO `t_company`(`id`, `username`, `name`, `tel`, `money`) VALUES (2, '300002', '顺丰快递', '13517771234', 10.00);
+INSERT INTO `t_company`(`id`, `username`, `name`, `tel`, `money`) VALUES (3, '300003', '圆通快递', '15544789901', 0.00);
---
+++ t_customer.sql
@@ -0,0 +1,2 @@
+INSERT INTO `t_customer`(`id`, `username`, `name`, `sex`, `tel`, `money`, `addr`) VALUES (1, '100001', '勒布朗', '男', '13278564423', 40690.32, '江西省南昌市南昌县昌东镇江西师范大学14栋');
+INSERT INTO `t_customer`(`id`, `username`, `name`, `sex`, `tel`, `money`, `addr`) VALUES (2, '100002', '艾丽丝', '女', '15957745624', 922.00, '上海市黄浦区南京西路455弄4号');
